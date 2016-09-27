215e2e7fcba9d4688ae53731ccfbb3a3e6240965
Davidson <davidsongoes@gmail.com>
2016-09-26 22:27:10 -0300

    Criando semeador da tabela homestead

---
+++ database/seeds/JobsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class JobsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      factory(\Jobs\Job::class)->times(100)->create();
+    }
+}
