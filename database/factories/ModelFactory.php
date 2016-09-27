cd5cc72d4ccc5402f5f5f78b76acae8ffa9e5dda
Davidson <davidsongoes@gmail.com>
2016-09-26 22:59:13 -0300

    refatorando o modelfactory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -17,7 +17,17 @@ $factory->define(Jobs\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
+        'phone' => $faker->phoneNumber,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(Jobs\Job::class, function (Faker\Generator $faker){
+    return [
+      'user_id' => mt_rand(1,27),
+        'title' => $faker->realText(100),
+        'description' => implode("\n", $faker->paragraphs),
+        'expires_at' => $faker->randomElements([null, $faker->date('Y-m-d')])
     ];
 });
