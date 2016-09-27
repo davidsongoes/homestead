375312b361d1adcd216dc7f21cb5b6b7abf20053
Davidson <davidsongoes@gmail.com>
2016-09-26 21:58:40 -0300

    Criando o semeador da tabela usuário

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i<27; $i++){
+            $user = new \Jobs\User();
+            $user->name = "User ($i)";
+            $user->email = "eser{$i}@gmail.com";
+            $user->phone = mt_rand(100000000, 999999999);
+            $user->save();
+        }
+    }
+}
